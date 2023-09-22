fb3e5af097dce987048ee6236f21966817d9fbbc
Joinas <jgoldhammer@outlook.de>
2023-09-22 19:42:35 +0200

    Fix out-of-bounds
    Final update for this problem

--- algorithms/IsSubsequence.java
+++ algorithms/IsSubsequence.java
@@ -2,23 +2,23 @@ public class IsSubsequence {
   public static boolean isSubsequence(String s, String t) {
     // if s is subsequence of t return true
     // else false
-    if (t.contains(s)){
-      return true;
-    }
     int count = 0;
     int j = 0;
     for (int i=0; i < t.length(); i++){
-        if (s.charAt(j) == (t.charAt(i))){
-          count++;
-          j++;
+        // obviously still needs the check for length
+        if (j < s.length()){
+          if (s.charAt(j) == (t.charAt(i))){
+            count++;
+            j++;
+            }
         }
     }
    return count == s.length();
 }
 
   public static void main(String[] args) {
-   String s = "acb";
-   String t = "ahbgdc";
+   String s = "leeeeetcode";
+   String t = "reeeeeeeeeeeeee";
    System.out.println(isSubsequence(s,t));
   }
 }
