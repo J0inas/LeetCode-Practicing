64909d0d0d03cdf1e1bfac556add249736a48e22
Joinas <jgoldhammer@outlook.de>
2023-09-26 15:14:59 +0200

    Add new problems

---
+++ algorithms/java/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+package java;
+
+public class AddTwoNumbers {
+    // idk first thing that came to mind was a stack
+    // on another thougth:
+    // you could add the list node for node  -> if the sum of list ...
+    // NO
+    // you could also just read the chars out, reverse them with a function and add them.
+    // SPECIAL CASE: 0;
+}
