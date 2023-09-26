90be8782f773c7811e5a2e6b533989478a68636b
Joinas <jgoldhammer@outlook.de>
2023-09-26 16:53:07 +0200

    Add note to AddTwoNumbers.java

--- algorithms/java/AddTwoNumbers.java
+++ algorithms/java/AddTwoNumbers.java
@@ -1,10 +1,22 @@
 package java;
 
+/* NOTES:
+ * I must admit, I did not understand this task at first.
+ * I had several thoughts starting out and then I had an algorithm that wasn't bad, 
+ * but returned an integer instead of a list.
+ * That was the point I decided to look at an explanation and found this: 
+ * https://leetcode.com/problems/add-two-numbers/solutions/1835535/java-c-a-very-beautiful-explanation-ever-exists/
+ * That 3 stepped helped a lot and proved my initial thought starting out:
+ * "you could add the list node for node -> sum of the list", but what about them 10's?
+ * That's what I want to solve now.
+ */
+
 public class AddTwoNumbers {
-    // idk first thing that came to mind was a stack
-    // on another thougth:
-    // you could add the list node for node  -> if the sum of list ...
-    // NO
-    // you could also just read the chars out, reverse them with a function and add them.
-    // SPECIAL CASE: 0;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int head = 0;
+        ListNode sum = new ListNode(head);
+        while (l1.next != null || l2.next != null){
+            if (    )
+        }
+    }
 }
